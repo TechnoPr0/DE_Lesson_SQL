921e1b93c0b60b029ef10240a93a6ccf2c56d254
Kirill <kiris2004@mail.ru>
2022-11-11 04:46:49 +0300

    add auto increment in tables

--- lesson_2_3.sql
+++ lesson_2_3.sql
@@ -1,83 +1,89 @@
 CREATE TABLE  level(
-    id serial PRIMARY KEY ,
-    title varchar(128) NOT NULL
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
+    title  VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE  department(
-    id serial PRIMARY KEY ,
-    title varchar(128) NOT NULL,
-    full_name_chief varchar(128) NOT NULL ,
-    number_of_employees integer NOT NULL
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
+    title VARCHAR(128) NOT NULL,
+    full_name_chief VARCHAR(128) NOT NULL ,
+    number_of_employees INT NOT NULL
 )
 
 CREATE TABLE staff(
-    id serial PRIMARY KEY,
-    full_name varchar(128) NOT NULL ,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name VARCHAR(128) NOT NULL ,
     birthday DATE NOT NULL ,
     start_work DATE NOT NULL ,
-    position varchar(128) NOT NULL ,
-    level_id integer REFERENCES level(id),
-    salary integer NOT NULL ,
-    departament_id integer REFERENCES  department(id),
-    driving_permit boolean
+    position VARCHAR(128) NOT NULL ,
+    level_id  INT REFERENCES level(id),
+    salary INT NOT NULL ,
+    departament_id INT REFERENCES  department(id),
+    driving_permit BOOLEAN
 );
 
 CREATE TABLE  score(
-    id serial PRIMARY KEY ,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
     staff_id integer REFERENCES staff(id),
-    first_quarter varchar(1) CHECK (score.first_quarter ~ '[A-E]'),
-    second_quarter varchar(1) CHECK (score.second_quarter ~ '[A-E]'),
-    third_quarter varchar(1) CHECK (score.third_quarter ~ '[A-E]'),
-    fourth_quarter varchar(1) CHECK (score.fourth_quarter ~ '[A-E]')
+    first_quarter VARCHAR(1) CHECK (score.first_quarter ~ '[A-E]'),
+    second_quarter VARCHAR(1) CHECK (score.second_quarter ~ '[A-E]'),
+    third_quarter VARCHAR(1) CHECK (score.third_quarter ~ '[A-E]'),
+    fourth_quarter VARCHAR(1) CHECK (score.fourth_quarter ~ '[A-E]')
 
 );
 
 ---------------------------------------------
 
 INSERT INTO level
+(title)
 VALUES
-    (1, 'Junior'),
-    (2, 'Middle'),
-    (3, 'Senior'),
-    (4, 'Lead');
+    ('Junior'),
+    ('Middle'),
+    ('Senior'),
+    ('Lead');
 
 INSERT INTO department
+(title, full_name_chief, number_of_employees)
 VALUES
-    (1, 'Отдел разработки', 'Галкин Александр Алексеевич', 3),
-    (2, 'Отдел тестирования', 'Павлова Полина Даниэльевна', 2);
+    ('Отдел разработки', 'Галкин Александр Алексеевич', 3),
+    ('Отдел тестирования', 'Павлова Полина Даниэльевна', 2);
 
 
 INSERT INTO staff
+(full_name, birthday, start_work, position, level_id, salary, departament_id, driving_permit)
 VALUES
-(1, 'Иванов Кирилл Егорович', '1989.01.20', '2009.02.02', 'Программист', 3, 190000, 1, 'True' ),
-(2, 'Галкин Александр Алексеевич', '1980.11.20', '2000.12.01', 'Руководитель отдела разработки', 4, 250000, 1, 'True'),
-(3, 'Беляев Михаил Денисович', '1990.05.10', '2010.09.02', 'Программист', 2, 150000, 1, 'False' ),
-(4, 'Королев Александр Александрович', '2000.03.25', '2019.04.15', 'Тестировщик', 1, 60000, 2, 'False'),
-(5, 'Павлова Полина Даниэльевна', '1988.05.29', '2010.10.03', 'Руководитель отдела тестирования', 4, 250000, 2, 'True');
+('Иванов Кирилл Егорович', '1989.01.20', '2009.02.02', 'Программист', 3, 190000, 1, 'True' ),
+('Галкин Александр Алексеевич', '1980.11.20', '2000.12.01', 'Руководитель отдела разработки', 4, 250000, 1, 'True'),
+('Беляев Михаил Денисович', '1990.05.10', '2010.09.02', 'Программист', 2, 150000, 1, 'False' ),
+('Королев Александр Александрович', '2000.03.25', '2019.04.15', 'Тестировщик', 1, 60000, 2, 'False'),
+('Павлова Полина Даниэльевна', '1988.05.29', '2010.10.03', 'Руководитель отдела тестирования', 4, 250000, 2, 'True');
 
 ---------------------------------------------
 
 INSERT INTO score
+(staff_id, first_quarter, second_quarter, third_quarter, fourth_quarter)
 VALUES
-(1, 1, 'A', 'A', 'B', 'B'),
-(2, 2, 'A', 'A', 'A', 'A'),
-(3, 3, 'C', 'D', 'B', 'A'),
-(4, 4, 'D', 'C', 'A', 'A'),
-(5, 5, 'A', 'B', 'B', 'A');
+(1, 'A', 'A', 'B', 'B'),
+(2, 'A', 'A', 'A', 'A'),
+(3, 'C', 'D', 'B', 'A'),
+(4, 'D', 'C', 'A', 'A'),
+(5, 'A', 'B', 'B', 'A');
 
 ---------------------------------------------
 
 INSERT INTO department
+(title, full_name_chief, number_of_employees)
 VALUES
-(3, 'Отдел Интелектуального анализа данных', 'Пахомова Екатерина Вадимовна', 3);
+('Отдел Интелектуального анализа данных', 'Пахомова Екатерина Вадимовна', 3);
 
 
 INSERT INTO  staff
+(full_name, birthday, start_work, position, level_id, salary, departament_id, driving_permit)
 VALUES
-(6, 'Пахомова Екатерина Вадимовна', '1990.04.12', '2022.09.20', 'Руководитель отлела Интелектуального анализа данных',
+('Пахомова Екатерина Вадимовна', '1990.04.12', '2022.09.20', 'Руководитель отлела Интелектуального анализа данных',
  4, 220000, 3, 'False'),
-(7, 'Киселева Майя Егоровна', '1995.06.24', '2022.10.15', 'Аналитик данных', 2, 150000, 3, 'False'),
-(8, 'Кузнецов Мирон Валерьевич', '1999.09.30', '2022.11.03', 'Аналитик данных', 1, 65000, 3, 'False');
+('Киселева Майя Егоровна', '1995.06.24', '2022.10.15', 'Аналитик данных', 2, 150000, 3, 'False'),
+('Кузнецов Мирон Валерьевич', '1999.09.30', '2022.11.03', 'Аналитик данных', 1, 65000, 3, 'False');
 
 ---------------------------------------------
 
